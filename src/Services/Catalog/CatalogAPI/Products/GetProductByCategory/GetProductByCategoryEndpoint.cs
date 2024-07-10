8f624b6370856a74026f9c973c2d2c02184d009a
N1kakk <veronikakolesova2003@gmail.com>
2024-07-10 19:22:32 +0300

    delete update - catalog

--- src/Services/Catalog/CatalogAPI/Products/GetProductByCategory/GetProductByCategoryEndpoint.cs
+++ src/Services/Catalog/CatalogAPI/Products/GetProductByCategory/GetProductByCategoryEndpoint.cs
@@ -1,12 +1,24 @@
 ﻿
+using CatalogAPI.Products.GetProductById;
+using System.Collections;
+
 namespace CatalogAPI.Products.GetProductByCategory;
 
-
+public record GetProductByCategoryResponse(IEnumerable<Product> Products);
 
 public class GetProductByCategoryEndpoint : ICarterModule
 {
 	public void AddRoutes(IEndpointRouteBuilder app)
 	{
-		throw new NotImplementedException();
+		app.MapGet("/products/sategory/{category}", async(string category, ISender sender) =>
+		{
+			var res = await sender.Send(new GetProductByCategoryQuery(category));
+			var response = res.Adapt<GetProductByCategoryResponse>();
+			return Results.Ok(response);
+		}).WithName("GetProductByCategory")
+		  .Produces<GetProductByCategoryResponse>(StatusCodes.Status200OK)
+		  .ProducesProblem(StatusCodes.Status400BadRequest)
+		  .WithSummary("Get Product by Category")
+		  .WithDescription("Get Product by Category");
 	}
 }
