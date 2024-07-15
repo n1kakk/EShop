fa1471f497bafdce991cde1eb6884dca2e8cffbd
N1kakk <veronikakolesova2003@gmail.com>
2024-07-15 22:46:41 +0300

    Adding Dependency Injection into ordering service

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,6 +1,12 @@
+using Ordering.API;
+using Ordering.Application;
+using Ordering.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 //Add services to the container
+builder.Services.AddApplicationServices().AddInfrastractureServices(builder.Configuration).AddApiServices();
+
 
 var app = builder.Build();
 
