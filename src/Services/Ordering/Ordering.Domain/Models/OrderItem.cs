516afc25fe022116a6a7d579f6467358e8839002
N1kakk <veronikakolesova2003@gmail.com>
2024-07-16 21:25:00 +0300

    Created value objects, rich domain model

---
+++ src/Services/Ordering/Ordering.Domain/Models/OrderItem.cs
@@ -0,0 +1,22 @@
+﻿
+using Ordering.Domain.Abstractions;
+using Ordering.Domain.ValueObjects;
+
+namespace Ordering.Domain.Models;
+
+public class OrderItem : Entity<OrderItemId>
+{
+    internal OrderItem(OrderId oredrId, ProductId productId, int quantity, decimal price)
+    {
+        Id = OrderItemId.Of(Guid.NewGuid());
+        OrderId = oredrId;
+        ProductId = productId;
+        Quantity = quantity;
+        Price = price;
+    }
+
+    public OrderId OrderId { get; private set; } = default!;
+	public ProductId ProductId { get; private set; } = default!;
+	public int Quantity { get; private set; } = default!;
+	public decimal Price { get; private set; } = default!;
+}
