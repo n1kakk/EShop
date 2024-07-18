a6540833678c7bfc22d4da1f3d63dd9b9e2e7cd8
N1kakk <veronikakolesova2003@gmail.com>
2024-07-18 19:53:23 +0300

    Ordering.Infrastructure: Dtos, IApplicationDbContext, Create order command&handler

---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderCommand.cs
@@ -0,0 +1,22 @@
+﻿using BuildingBlocks.CQRS;
+using FluentValidation;
+using Ordering.Application.Dtos;
+using System.Windows.Input;
+
+namespace Ordering.Application.Orders.Commands.CreateOrder;
+
+public record CreateOrderCommand(OrderDto OrderDto)
+	:ICommand<CreateOrderResult>;
+
+public record CreateOrderResult(Guid Id);
+
+
+public class CreateOrderCommandValidator : AbstractValidator<CreateOrderCommand>
+{
+	public CreateOrderCommandValidator()
+	{
+		RuleFor(x => x.OrderDto.OrderName).NotEmpty().WithMessage("Name is required");
+		RuleFor(x => x.OrderDto.CustomerId).NotNull().WithMessage("CustomerId is required");
+		RuleFor(x => x.OrderDto.OrderItems).NotEmpty().WithMessage("OrderItems should not be empty");
+	}
+}
