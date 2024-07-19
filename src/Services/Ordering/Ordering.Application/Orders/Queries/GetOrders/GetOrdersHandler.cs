5862381abc5d210c46e9ac414fe465014e738dfe
N1kakk <veronikakolesova2003@gmail.com>
2024-07-19 19:13:51 +0300

    Get orders. get irders by customer, pagination

---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrders/GetOrdersHandler.cs
@@ -0,0 +1,31 @@
+﻿using BuildingBlocks.CQRS;
+using BuildingBlocks.Pagination;
+using Microsoft.EntityFrameworkCore;
+using Ordering.Application.Data;
+using Ordering.Application.Dtos;
+using Ordering.Application.Extensions;
+
+namespace Ordering.Application.Orders.Queries.GetOrders;
+
+public class GetOrdersHandler(IApplicationDbContext context)
+	: IQueryHandler<GetOrdersQuery, GetOrdersResult>
+{
+	public async Task<GetOrdersResult> Handle(GetOrdersQuery query, CancellationToken cancellationToken)
+	{
+		var pageIndex = query.PaginationRequest.PageIndex;
+		var pageSize = query.PaginationRequest.PageSize;
+
+		var totalCount = await context.Orders.LongCountAsync(cancellationToken);
+
+		var orders = await context.Orders
+			.Include(o => o.OrderItems)
+			.OrderBy(o => o.OrderName.Value)
+			.Skip(pageSize * pageIndex)
+			.Take(pageSize)
+			.ToListAsync(cancellationToken);
+
+		return new GetOrdersResult(
+			new PaginatedResult<OrderDto>(pageIndex, pageSize,totalCount, orders.ToOrdetDtoList()));
+
+	}
+}
