53579e8784850094ff731da51d15bb9f6a732edb
N1kakk <veronikakolesova2003@gmail.com>
2024-07-19 17:23:31 +0300

    extension method

--- src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByName/GetOrdersByNaneHandler.cs
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByName/GetOrdersByNaneHandler.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Ordering.Application.Data;
 using Ordering.Application.Dtos;
+using Ordering.Application.Extensions;
 using Ordering.Domain.Models;
 
 namespace Ordering.Application.Orders.Queries.GetOrdersByName;
@@ -18,29 +19,9 @@ public class GetOrdersByNaneHandler(IApplicationDbContext context)
 			   .OrderBy(o => o.OrderName)
 			   .ToListAsync(cancellationToken);
 
-		var orderDtos = ProjectToOrderDto(orders);
-
-		return new GetOrdersByNameResult(orderDtos);
+		return new GetOrdersByNameResult(orders.ToOrdetDtoList());
 	}
 
-	private List<OrderDto> ProjectToOrderDto(List<Order> orders)
-	{
-		List<OrderDto> result = new();
-		foreach(var order in orders)
-		{
-			var orderDto = new OrderDto(
-			Id: order.Id.Value,
-			CustomerId: order.CustomerId.Value,
-			OrderName: order.OrderName.Value,
-			ShippingAddress: new AddressDto(order.ShippingAddress.FirstName, order.ShippingAddress.LastName, order.ShippingAddress.EmailAddress!, order.ShippingAddress.AddressLine, order.ShippingAddress.Country, order.ShippingAddress.State, order.ShippingAddress.ZipCode),
-			BillingAddress: new AddressDto(order.BillingAddress.FirstName, order.BillingAddress.LastName, order.BillingAddress.EmailAddress!, order.BillingAddress.AddressLine, order.BillingAddress.Country, order.BillingAddress.State, order.BillingAddress.ZipCode),
-			Payment: new PaymentDto(order.Payment.CardName!, order.Payment.CardNumber, order.Payment.Expiration, order.Payment.CVV, order.Payment.PaymentMethod),
-			Status: order.Status,
-			OrderItems: order.OrderItems.Select(oi => new OrderItemDto(oi.OrderId.Value, oi.ProductId.Value, oi.Quantity, oi.Price)).ToList());
 
-			result.Add(orderDto);   
-		}
-		return result;
-		
-	}
+
 }
