aab65b96fa4c0f046537db298cd9562d0bba8adf
N1kakk <veronikakolesova2003@gmail.com>
2024-07-21 19:03:32 +0300

    OrderingAPI: registerimg massTransit

--- src/Services/Ordering/Ordering.Application/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Application/DependencyInjection.cs
@@ -1,5 +1,7 @@
 ﻿
 using BuildingBlocks.Behaviors;
+using BuildingBlocks.Messaging.MassTransit;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
 
@@ -7,7 +9,8 @@ namespace Ordering.Application;
 
 public static class DependencyInjection
 {
-	public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+	public static IServiceCollection AddApplicationServices
+		(this IServiceCollection services, IConfiguration configuration)
 	{
 		services.AddMediatR(cfg =>
 		{
@@ -15,6 +18,9 @@ public static class DependencyInjection
 			cfg.AddOpenBehavior(typeof(ValidationBehavior<,>));
 			cfg.AddOpenBehavior(typeof(LoggingBehavior<,>));
 		});
+
+		services.AddMessageBroker(configuration, Assembly.GetExecutingAssembly());
+
 		return services;
 	}
 }
