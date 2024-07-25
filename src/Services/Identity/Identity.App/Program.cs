9dc5ba7df31c9daa992d0ff6785c85f42f646891
N1kakk <veronikakolesova2003@gmail.com>
2024-07-25 20:05:26 +0300

    Development - Database.EnsureDeleted();         dbContext.Database.EnsureCreated();

--- src/Services/Identity/Identity.App/Program.cs
+++ src/Services/Identity/Identity.App/Program.cs
@@ -47,7 +47,18 @@ if (!app.Environment.IsDevelopment())
 	app.UseExceptionHandler("/Error");
 	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 	app.UseHsts();
+
 }
+if (app.Environment.IsDevelopment())
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+        dbContext.Database.EnsureDeleted();
+        dbContext.Database.EnsureCreated();
+    }
+}
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
